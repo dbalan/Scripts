38e9b5d6ff9d4d74e811dd26659d71f497f524a8
Dhananjay M Balan <mb.dhananjay@gmail.com>
2011-09-15 09:18:45 +0530

    Parses an html file and downloads specific file links.

--- download/download.sh
+++ download/download.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
-# This script downloads the links in the file $SRC. Written to be used as a cron job, but still has to hack. 
-# As of now this is same as 
-# 	wget -ic $SRC
-#
+# This script downloads the links in the html file given as argument.
+#  
 
 SRC=links
+URL=$1
+
+TYPE=avi  # File type to download.
+
+if [[ ! -r $URL ]]; then
+	echo E: No input file found!!
+	echo Usage: $0 <html file>
+	exit -1
+else
+	./list_links.sed $URL | grep *.$TYPE > $SRC 
+fi
 
 if [[ ! -r $SRC ]]; then
 	echo E: File not found!!
 	exit -1
 fi
 
-echo I: All good Starting download.
+echo I: All good, Starting download.
 
 N=0
 cat $SRC | while read LINK; do
@@ -20,6 +29,7 @@ cat $SRC | while read LINK; do
 	N=$((N+1))
 	wget -c "$LINK"
 	done
-
+echo I: Download complete, Cleaning Up.
+rm $SRC
 exit 0
 
